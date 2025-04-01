cdc91b2f8c7dec030d01276141aee2ba0d4b719a
Saurabh Kumar <skdubey568@gmail.com>
2025-04-01 18:00:45 +0530

    Add files via upload

---
+++ real.py
@@ -0,0 +1,12 @@
+import google.generativeai as ai
+API_KEY='AIzaSyDryvfzmXv0MzjBN3myX3O6fyGgp3QJrTc'
+ai.configure(api_key=API_KEY)
+model= ai.GenerativeModel("gemini-1.5-pro")
+chat= model.start_chat()
+while True:
+    message= input('You: ')
+    if message.lower()=='bye':
+       print('Chatbot: Goodbye!')
+       break
+    response= chat.send_message(message)
+    print('Chatbot:', response.text)
